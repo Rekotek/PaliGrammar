aae2dbc6be6873ae05954935ce811ac8a479949b
Taras Tkachenko <TarasTkachenko@me.com>
2021-12-28 21:35:43 +0200

    Initial Commit

---
+++ src/main/java/com/scriptorium/pali/enums/Gender.java
@@ -0,0 +1,11 @@
+package com.scriptorium.pali.enums;
+
+public enum Gender {
+    MUSCLE("m"),
+    NEUTRAL("n"),
+    FEMALE("f");
+
+    Gender(String symbol) {
+    }
+
+}
---
+++ src/main/java/com/scriptorium/pali/enums/NumberType.java
@@ -0,0 +1,9 @@
+package com.scriptorium.pali.enums;
+
+public enum NumberType {
+    SG("sg", "singular", "ед. число"),
+    PL("pl", "pluralis", "мн. число");
+
+    NumberType(String symbol, String longDefinition, String rusDefinition) {
+    }
+}
