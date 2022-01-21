fe31005b24370b12e096a37b545e9745a2dd22e0
Taras Tkachenko <TarasTkachenko@me.com>
2022-01-21 21:38:47 +0200

    Add ToString for NounDescription record

--- src/main/java/com/scriptorium/pali/common/NounDescription.java
+++ src/main/java/com/scriptorium/pali/common/NounDescription.java
@@ -4,4 +4,8 @@ import com.scriptorium.pali.enums.NumberType;
 import com.scriptorium.pali.enums.WordCase;
 
 public record NounDescription(WordCase wordCase, NumberType numberType) {
+    @Override
+    public String toString() {
+        return "[" + wordCase.getSymbol() +", " + numberType.toString() + "]";
+    }
 }
